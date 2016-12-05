170f844770d61367f62391ac79edab33bb2c39f7
cesare.soldini <cesare.soldini@accurat.it>
2016-12-05 20:40:50 +0100

    New files structure in app template

--- packages/accurapp-scripts/template/src/containers/App/index.js
+++ packages/accurapp-scripts/template/src/containers/App/index.js
@@ -3,11 +3,11 @@ import React from 'react'
 export default class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
+      <div>
+        <h2>
+          IBM QRadar
+        </h2>
+        <p>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
