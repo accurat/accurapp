2802a3f63fd816310a927679b52505a4ac37896a
cesare.soldini <cesare.soldini@accurat.it>
2017-04-21 16:50:41 +0200

    ⚙️🚨 Closes #1 on smarter red errors

--- packages/eslint-config-accurapp/index.js
+++ packages/eslint-config-accurapp/index.js
@@ -66,65 +66,25 @@ module.exports = {
     'react/jsx-indent': [1, 2],
     'react/jsx-indent-props': [1, 2],
     'react/jsx-wrap-multilines': 1,
+    'react/jsx-equals-spacing': [1, 'never'],
+    'react/jsx-no-duplicate-props': [1, { ignoreCase: true }],
+    'react/jsx-no-undef': 1,
+    'react/jsx-pascal-case': [1, { allowAllCaps: true, ignore: [] }],
+    'react/jsx-uses-react': 1,
+    'react/jsx-uses-vars': 1,
+    'react/no-danger-with-children': 1,
+    'react/no-deprecated': 1,
+    'react/no-direct-mutation-state': 1,
+    'react/no-is-mounted': 1,
+    'react/require-render-return': 1,
+    'react/style-prop-object': 1,
     // ERRORS
+    'no-redeclare': 2,
+    'no-undef': 2,
+    'no-unexpected-multiline': 2,
+    'no-use-before-define': [2, { functions: false, classes: true, variables: true }],
     'react/no-unused-prop-types': 2,
-    'react/jsx-no-bind': [2, {
-      ignoreRefs: true,
-    }],
-
-    // *** These rules are taken from eslint-config-react-app ***
-
-    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/
-
-    // TODO: import rules are temporarily disabled because they don't play well
-    // with how eslint-loader only checks the file you change. So if module A
-    // imports module B, and B is missing a default export, the linter will
-    // record this as an issue in module A. Now if you fix module B, the linter
-    // will not be aware that it needs to re-lint A as well, so the error
-    // will stay until the next restart, which is really confusing.
-
-    // This is probably fixable with a patch to eslint-loader.
-    // When file A is saved, we want to invalidate all files that import it
-    // *and* that currently have lint errors. This should fix the problem.
-    // (As an exception, import/no-webpack-loader-syntax can be enabled already
-    // because it doesn't depend on whether the file exists, so this issue
-    // doesn't apply to it.)
-
-    // 'import/default': 'warn',
-    // 'import/export': 'warn',
-    // 'import/named': 'warn',
-    // 'import/namespace': 'warn',
-    // 'import/no-amd': 'warn',
-    // 'import/no-duplicates': 'warn',
-    // 'import/no-extraneous-dependencies': 'warn',
-    // 'import/no-named-as-default': 'warn',
-    // 'import/no-named-as-default-member': 'warn',
-    // 'import/no-unresolved': ['warn', { commonjs: true }],
-    // We don't support configuring Webpack using import source strings, so this
-    // is always an error.
-    'import/no-webpack-loader-syntax': 'error',
-
-    // https://github.com/yannickcr/eslint-plugin-react/tree/master/docs/rules
-    'react/jsx-equals-spacing': ['warn', 'never'],
-    'react/jsx-no-duplicate-props': ['warn', { ignoreCase: true }],
-    'react/jsx-no-undef': 'warn',
-    'react/jsx-pascal-case': ['warn', {
-      allowAllCaps: true,
-      ignore: [],
-    }],
-    'react/jsx-uses-react': 'warn',
-    'react/jsx-uses-vars': 'warn',
-    'react/no-danger-with-children': 'warn',
-    'react/no-deprecated': 'warn',
-    'react/no-direct-mutation-state': 'warn',
-    'react/no-is-mounted': 'warn',
-    'react/react-in-jsx-scope': 'error',
-    'react/require-render-return': 'warn',
-    'react/style-prop-object': 'warn',
-
-    // https://github.com/gajus/eslint-plugin-flowtype
-    'flowtype/define-flow-type': 'warn',
-    'flowtype/require-valid-file-annotation': 'warn',
-    'flowtype/use-flow-type': 'warn'
-  })
-};
+    'react/jsx-no-bind': [2, { ignoreRefs: true }],
+    'react/react-in-jsx-scope': 2,
+  }),
+}
