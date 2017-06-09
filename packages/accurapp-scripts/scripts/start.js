362ab50d70bb398b97745b8e0ccd65e286d8f145
Marco Fugaro <marco.fugaro@gmail.com>
2017-06-09 17:13:25 +0200

    📶  Add lan url to start script, fixes #11

--- packages/accurapp-scripts/scripts/start.js
+++ packages/accurapp-scripts/scripts/start.js
@@ -8,34 +8,41 @@ const chalk = require('chalk')
 const detect = require('detect-port')
 const WebpackDevServer = require('webpack-dev-server')
 const openOrRefreshBrowser = require('react-dev-utils/openBrowser')
+const { prepareUrls } = require('react-dev-utils/WebpackDevServerUtils')
 const { log, createWebpackCompiler, coloredBanner } = require('./_utils')
 
-const HOST = process.env.HOST || 'localhost'
+const HOST = process.env.HOST || '0.0.0.0'
 const DEFAULT_PORT = parseInt(process.env.PORT, 10) || 8000
 
-const devServerConfig = {
-  compress: true,
-  clientLogLevel: 'none',
-  historyApiFallback: true,
-  hot: true,
-  contentBase: './public/',
-  quiet: true,
-  watchOptions: {
-    ignored: /node_modules/,
-  },
-  host: HOST,
-  overlay: false,
+function buildDevServerConfig(lanUrl) {
+  return {
+    compress: true,
+    clientLogLevel: 'none',
+    historyApiFallback: true,
+    hot: true,
+    contentBase: './public/',
+    quiet: true,
+    watchOptions: {
+      ignored: /node_modules/,
+    },
+    host: HOST,
+    overlay: false,
+    public: lanUrl,
+  }
 }
 
 function runDevServer(port) {
+  const urls = prepareUrls('http', HOST, port)
   const compiler = createWebpackCompiler(() => {
-    log.info(`The app is running at: ${chalk.cyan(`http://${HOST}:${port}/`)}`)
+    log.info(`The app is running at: ${chalk.cyan(urls.localUrlForTerminal)}`)
+    log.info(`Or on your network at: ${chalk.cyan(urls.lanUrlForTerminal)}`)
   })
+  const devServerConfig = buildDevServerConfig(urls.lanUrlForConfig)
   const devServer = new WebpackDevServer(compiler, devServerConfig)
   devServer.listen(port, err => {
     if (err) return log.err(err)
     log.info('Starting the development server...')
-    openOrRefreshBrowser(`http://${HOST}:${port}/`)
+    openOrRefreshBrowser(urls.localUrlForBrowser)
   })
 }
 
