a41ffe8a09ff6c0ec6fb9ff1ea0402b374daa468
Marco Fugaro <marco.fugaro@gmail.com>
2018-08-28 18:53:22 +0200

    📦 Upgrade eslint and scripts packages

--- packages/eslint-config-accurapp/index.js
+++ packages/eslint-config-accurapp/index.js
@@ -43,6 +43,14 @@ module.exports = {
     'no-copy-paste-default-export',
   ],
 
+  settings: {
+    react: {
+      // Force the react version, otherwise it will log an error to the console
+      // https://github.com/yannickcr/eslint-plugin-react/issues/1955
+      version: '999.999.999',
+    },
+  },
+
   env: {
     es6: true,
     node: true,
