c75f5627fcff98dea623bde849431edaae11a312
Marco Fugaro <marco.fugaro@gmail.com>
2019-05-02 14:39:49 +0200

    🐛  Replace prettier with prettier-eslint because it was conflicting

--- packages/accurapp-scripts/scripts/prettier.js
+++ packages/accurapp-scripts/scripts/prettier.js
@@ -1,7 +1,7 @@
 const cp = require('child_process')
 
 const args = process.argv.slice(process.argv.indexOf('prettier') + 1)
-const prettier = require.resolve('prettier/bin-prettier')
+const prettier = require.resolve('prettier-eslint-cli/dist/index.js')
 
 cp.execSync(
   `${prettier} ${args.length > 0 ? args.join(' ') : '--write'} 'src/**/*.{js,jsx,ts,tsx}'`,
